645f86ca55ec7692f6185d74b4b40465988e9be8
giomerida <giomerida@icloud.com>
2023-06-28 21:26:33 -0600

    se agrega el proyecto

---
+++ enviar_formulario.php
@@ -0,0 +1,24 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  $nombre = $_POST['nombre'];
+  $correo = $_POST['correo'];
+  $asunto = $_POST['asunto'];
+  $mensaje = $_POST['mensaje'];
+
+  $destinatario = 'giomerida@cuttux.com';
+  $asuntoCorreo = 'Nuevo mensaje del formulario de contacto';
+
+  $contenido = "Nombre: $nombre\n";
+  $contenido .= "Correo electrónico: $correo\n";
+  $contenido .= "Asunto: $asunto\n";
+  $contenido .= "Mensaje: $mensaje\n";
+
+  $cabeceras = "From: $nombre <$correo>";
+
+  if (mail($destinatario, $asuntoCorreo, $contenido, $cabeceras)) {
+    echo 'Mensaje enviado con éxito.';
+  } else {
+    echo 'Error al enviar el mensaje. Por favor, intenta nuevamente.';
+  }
+}
+?>
---
+++ validacion.js
@@ -0,0 +1,40 @@
+//Haz tú validación en javascript acá
+document.getElementById('form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Evita el envío del formulario por defecto
+  
+    // Obtén los valores de los campos del formulario
+    var nombre = document.getElementById('nombre').value;
+    var correo = document.getElementById('correo').value;
+    var asunto = document.getElementById('asunto').value;
+    var mensaje = document.getElementById('mensaje').value;
+  
+    // Realiza la validación de los campos del formulario
+    if (nombre === '' || correo === '' || asunto === '' || mensaje === '') {
+      alert('Por favor, completa todos los campos del formulario.');
+      return;
+    }
+  
+    // Construye el cuerpo del mensaje de correo electrónico
+    var contenido = 'Nombre: ' + nombre + '\n';
+    contenido += 'Correo electrónico: ' + correo + '\n';
+    contenido += 'Asunto: ' + asunto + '\n';
+    contenido += 'Mensaje: ' + mensaje;
+  
+    // Envía el correo electrónico utilizando una solicitud AJAX
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', 'enviar_formulario.php', true);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        alert('Mensaje enviado con éxito.');
+        // Restablece los valores del formulario
+        document.getElementById('formulario').reset();
+      } else if (xhr.readyState === 4 && xhr.status !== 200) {
+        alert('Error al enviar el mensaje. Por favor, intenta nuevamente.');
+      }
+    };
+    xhr.send('nombre=' + encodeURIComponent(nombre) +
+             '&correo=' + encodeURIComponent(correo) +
+             '&asunto=' + encodeURIComponent(asunto) +
+             '&mensaje=' + encodeURIComponent(mensaje));
+  });
